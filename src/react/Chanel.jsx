1dae95a26f770722db2dfc4217d553b30363b298
mbalyura <m.balyura@gmail.com>
2020-05-03 00:07:27 +0300

    layout prepare

--- src/react/Chanel.jsx
+++ src/react/Chanel.jsx
@@ -7,12 +7,13 @@ export default class Chanel extends React.Component {
     const { data: { channels, currentChannelId } } = this.props;
 
     return (
-      <div className="chanels">
+      <div className="chanels w-100 mr-3">
         <ListGroup>
           {channels.map((item) => (
             <ListGroup.Item
               active={item.id === currentChannelId}
               key={item.id}
+              className="w-100"
             >
               {item.name}
             </ListGroup.Item>
--- src/react/index.jsx
+++ src/react/index.jsx
@@ -3,18 +3,41 @@ import ReactDOM from 'react-dom';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 import Chanel from './Chanel';
 
 export default (initState, userName) => {
   const dom = (
     <>
-      <Row />
-      <Row>
-        <Col md={3}>
-          <Chanel data={initState} />
+      <Row className="h-100">
+        <Col className="aside" xs={3}>
+          <Row className="chanels-menu mx-auto mb-3">
+            <h4 className="my-2">Chanels</h4>
+            <Button className="ml-auto">+</Button>
+          </Row>
+          <Row className="chanels">
+            <Chanel data={initState} />
+          </Row>
         </Col>
-        <Col md={9}>{userName}</Col>
+        <Col className="section d-flex flex-column">
+          <div className="messages-container overflow-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate, unde id. Velit officiis sunt possimus, autem
+            dolores iure, eius rem similique molestias maxime soluta
+            dolore quidem nostrum dolor hic error.
+          </div>
+          <div className="form-container mt-auto mb-4 w-100">
+            <Form className="">
+              <Form.Label>
+                {userName}
+                :
+              </Form.Label>
+              <Form.Control type="text" />
+            </Form>
+          </div>
+        </Col>
       </Row>
     </>
   );
