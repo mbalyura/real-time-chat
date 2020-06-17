e5b56f442ee03b10c4f7d42608f5650e894470f2
Max <Max@max.com>
2020-06-17 14:38:36 +0300

    move sockets to init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import socket from 'socket.io-client';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 import ChannelsList from './ChannelsList';
@@ -8,47 +6,26 @@ import ChannelsMenu from './ChannelsMenu';
 import Messages from './Messages';
 import NewMessageForm from './NewMessageForm';
 import Toaster from './Toaster';
-import { showSuccesToast, showDangerToast } from '../toasts';
 
-import {
-  addMessageSucces,
-  addChannelSucces,
-  renameChannelSucces,
-  removeChannelSucces,
-} from '../slices';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    socket()
-      .on('connect', () => showSuccesToast('alerts.connected'))
-      .on('disconnect', () => showDangerToast('alerts.disconnected'))
-      .on('newMessage', ({ data: { attributes } }) => dispatch(addMessageSucces({ message: attributes })))
-      .on('newChannel', ({ data: { attributes } }) => dispatch(addChannelSucces({ channel: attributes })))
-      .on('renameChannel', ({ data: { attributes } }) => dispatch(renameChannelSucces({ channel: attributes })))
-      .on('removeChannel', ({ data: { id } }) => dispatch(removeChannelSucces({ id })));
-  });
-
-  return (
-    <>
-      <Row className="mt-5">
-        <ChannelsMenu />
-      </Row>
-      <Row className="h-75">
-        <Col md={3}>
-          <ChannelsList />
-        </Col>
-        <Col className="d-flex flex-column h-100" md={9}>
-          <Messages />
-          <NewMessageForm />
-        </Col>
-      </Row>
-      <Row>
-        <Toaster />
-      </Row>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Row className="mt-5">
+      <ChannelsMenu />
+    </Row>
+    <Row className="h-75">
+      <Col md={3}>
+        <ChannelsList />
+      </Col>
+      <Col className="d-flex flex-column h-100" md={9}>
+        <Messages />
+        <NewMessageForm />
+      </Col>
+    </Row>
+    <Row>
+      <Toaster />
+    </Row>
+  </>
+);
 
 export default App;
--- src/init.jsx
+++ src/init.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { internet } from 'faker/locale/en/';
 import cookies from 'js-cookie';
+import socket from 'socket.io-client';
 import './i18n';
 import gon from 'gon';
 
+import rootReducer, {
+  addMessageSucces,
+  addChannelSucces,
+  renameChannelSucces,
+  removeChannelSucces,
+} from './slices';
+import { showSuccesToast, showDangerToast } from './toasts';
 import NameContext from './context';
-import rootReducer from './slices';
 import configStore from './configStore';
 import App from './components/App';
 
@@ -22,7 +29,18 @@ export default () => {
     },
     messagesInfo: { messages: gon.messages },
   };
+
   const store = configStore(rootReducer, preloadedState);
+
+  const { dispatch } = store;
+
+  socket()
+    .on('connect', () => showSuccesToast('alerts.connected'))
+    .on('disconnect', () => showDangerToast('alerts.disconnected'))
+    .on('newMessage', ({ data: { attributes } }) => dispatch(addMessageSucces({ message: attributes })))
+    .on('newChannel', ({ data: { attributes } }) => dispatch(addChannelSucces({ channel: attributes })))
+    .on('renameChannel', ({ data: { attributes } }) => dispatch(renameChannelSucces({ channel: attributes })))
+    .on('removeChannel', ({ data: { id } }) => dispatch(removeChannelSucces({ id })));
 
   ReactDOM.render(
     <Provider store={store}>
